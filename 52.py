b9d2931eeccf12808a179469f142196f0ebf1c46
saiharsh976 <51949944+saiharsh976@users.noreply.github.com>
2019-07-03 20:00:46 +0530

    Update 52.py

--- 52.py
+++ 52.py
@@ -1,3 +1,3 @@
 abc1=int(input))
-cm=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
+cm=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"]
 print(cm[abc1])
