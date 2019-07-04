6add54d6324a7a4e508721eeee58f42f9d70ed8f
saiharsh976 <51949944+saiharsh976@users.noreply.github.com>
2019-07-04 10:56:59 +0530

    Update 58.py

--- 58.py
+++ 58.py
@@ -1,9 +1,7 @@
 dm1,cd2=map(int,input().split())
 s=list(map(int,input().slpit()))
-count=0
-for i in s:
-    if(i==cd2):
-        print("yes")
-        break
-    else:
-        print("no")
+if(i==cd2):
+    print("yes")
+    break
+else:
+    print("no")
